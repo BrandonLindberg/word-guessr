e4914a571fc8815c3a77ce8bcddb7bf82860be14
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-06-13 09:36:32 -0600

    updated scripts

--- src/python-srv/build_answers.py
+++ src/python-srv/build_answers.py
@@ -5,17 +5,24 @@ nlp = spacy.load("en_core_web_sm")
 with open("answers.txt") as f:
     words = [line.strip() for line in f if line.strip()]
 
+print(f"Loaded {len(words)} words")
+print(words[:10])
+
 approved=[]
+
+allowed_pos = {"NOUN", "PROPN", "ADJ", "VERB"}
 
 for word in words:
     doc = nlp(word)
 
+    print(f"\nWord: {word}")
+    for token in doc:
+        print(f"Token: {token.text} | POS: {token.pos_}")
+
     valid = True
 
-    for token in doc:
-        if token.pos_ not in ["NOUN", "PROPN", "ADJ"]:
-            valid = False
-            break
+    if all(token.pos_ in allowed_pos for token in doc):
+        approved.append(word)
 
 with open("answers_filtered.text", "w") as f:
     for word in approved:
