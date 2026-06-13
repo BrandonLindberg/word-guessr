21ef9f289cafb295cf206edf0d1c0f512f822c23
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-06-13 16:55:44 -0600

    crazy updates

--- src/scripts/guessr.js
+++ src/scripts/guessr.js
@@ -1,16 +1,49 @@
 document.getElementById('guess-button').addEventListener('click', buttonQuery);
-document.getElementById('guess-input')
+
+let attemptCounter = 0;
 
 async function buttonQuery() {
-    const input = document.getElementById('guess-input')
+    const input = document.getElementById('guess-input');
     let guess = input.value.trim();
+    input.value = '';
     
     if (!guess) return;
 
+    attemptCounter++;
+    const counter = document.getElementById('attempts-text');
+    counter.innerHTML = `Attempts: ${attemptCounter}`;
+
     const response = await fetch(`/crunch?word=${encodeURIComponent(guess)}`);
 
-    input.value = '';
-
     const data = await response.json();
-    console.log(data);
+    queryResult(data.score, guess);
+
+    // console.log(data);
 };
+
+function queryResult(result, guess) {
+    const resultText = document.getElementById('result-num');
+    const resultNum = result;
+
+    if (resultNum <= 0.2){
+        resultText.innerHTML = 'Freezing';
+    }
+
+    else if (resultNum <= 0.4){
+        resultText.innerHTML = 'Cold';
+    }
+
+    else if (resultNum <= 0.6){
+        resultText.innerHTML = 'Lukewarm';
+    }
+
+    else if (resultNum <= 0.8){
+        resultText.innerHTML = 'Hot';
+    }
+    else if (resultNum < 0.99){
+        resultText.innerHTML = 'Burning Up';
+    }
+    else {
+        resultText.innerHTML = `Correct! Word was: ${guess}`;
+    }
+}
