391e98727cb6e4fc9c6f70da6a2dcca1161c6bc6
Brandon Lindberg <brandon.s.lindberg@gmail.com>
2026-06-13 11:06:37 -0600

    updated path

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const app = express();
 
 app.use(express.static('src'));
 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(3344, () => {
+    console.log('Server is running on port 3344');
 });
